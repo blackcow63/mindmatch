5131fac27555b7913add6d5d74d228ecf1871fd1
Zofia Smolen <zofsmolen@gmail.coim>
2023-04-24 21:46:51 +0200

    ""

--- streamlite_chatbot.py
+++ streamlite_chatbot.py
@@ -11,7 +11,7 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 def generate_response(prompt):
     completions = openai.Completion.create(
         engine = "gpt-3.5-turbo",
-        prompt_prefix = "Jesteś psychoterapeutą. Odpowiedz pacjentowi na tą wiadomość:" + prompt
+        prompt_prefix = "Jesteś psychoterapeutą. Odpowiedz pacjentowi na tą wiadomość w 2 osobie liczby pojedyńczej:" + "'" +prompt + "'",
         prompt = prompt,
         max_tokens = 400,
         n = 1,

4e55053f915f0599f2edbc606cfca23b03c9f110
Zofia Smolen <zofsmolen@gmail.coim>
2023-04-26 19:37:49 +0200

    update

--- streamlite_chatbot.py
+++ streamlite_chatbot.py
@@ -5,20 +5,27 @@ import streamlit as st
 from streamlit_chat import message
 st.title('Virtual Therapist')
 
+#st.set_page_config(
+#    page_title="Streamlit Chat - Demo",
+#    page_icon=":robot:"
+#)
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
+#st.header("Streamlit Chat - Demo")
+#st.markdown("[Github](https://github.com/ai-yash/st-chat)")
 
 def generate_response(prompt):
     completions = openai.Completion.create(
         engine = "gpt-3.5-turbo",
-        prompt_prefix = "You are a therapist. Reply in Polish AS A THERAPIST to this message:" + "'" +prompt + "Co mogę z tym zrobić?" + "'",
+        prompt_prefix = "Jesteś terpeutą. Odpowiedz na tą wiadomość tak aby dowiedzieć się jak najwięcej o objawach:" + "'" + prompt + "'",
         prompt = prompt,
         max_tokens = 400,
         n = 1,
         stop = None,
-        temperature=0.5,
+        temperature=0.7,
     )
-    message = "a" #completions.choices[0].text
+    message = completions.choices[0].text
+    print(message)
     return message
 
 # Storing the chat
@@ -27,6 +34,11 @@ if 'generated' not in st.session_state:
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
+
+#def query(payload):
+#	response = requests.post(API_URL, headers=headers, json=payload)
+#	return response.json()
+
 
 #TEST
 # We will get the user's input by calling the get_text function
